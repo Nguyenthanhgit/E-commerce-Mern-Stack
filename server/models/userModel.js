fa82e5cae7829c4cd0ffd68b3495d9de60789e3e
LAPTOP-8SCBNKRF\Admin <Nguyen Thanh>
2023-04-05 20:03:20 +0700

    first commit

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'; // Erase if already required
+
+// Declare the Schema of the Mongo model
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+const User = mongoose.model('User', userSchema);
+export default User;
